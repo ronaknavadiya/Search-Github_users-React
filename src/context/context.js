e2897e5a0e12465c273792846faa942e4a0470e7
ronaknavadiya <ronaknavadiya123@gmail.com>
2021-02-27 09:41:34 +0530

    info component done

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.js/mockUser';
-import mockRepos from './mockData.js/mockRepos';
-import mockFollowers from './mockData.js/mockFollowers';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import mockUser from "./mockData.js/mockUser";
+import mockRepos from "./mockData.js/mockRepos";
+import mockFollowers from "./mockData.js/mockFollowers";
+import axios from "axios";
 
-const rootUrl = 'https://api.github.com';
+const rootUrl = "https://api.github.com";
+
+const GithubContext = React.createContext();
+
+const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [folllowers, setFolllowers] = useState(mockFollowers);
+  const [repos, setRepos] = useState(mockRepos);
+
+  return (
+    <GithubContext.Provider value={{ githubUser, folllowers, repos }}>
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+const useGlobalContext = () => {
+  return React.useContext(GithubContext);
+};
+
+export { useGlobalContext, GithubProvider };
